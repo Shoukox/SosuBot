9f4becf81aed16dba33c2ac67e56a9f7f5aa14ff
Shoukko <shachzod2004@gmail.com>
2024-06-28 23:58:38 +0200

    Update TextDatabase

--- Sosu_remaster_web/Services/TextDatabase.cs
+++ Sosu_remaster_web/Services/TextDatabase.cs
@@ -17,22 +17,32 @@ namespace Sosu.Services
         }
         public static void LoadData()
         {
-            if (!File.Exists("osuusers.txt"))
-                File.Create("osuusers.txt").Close();
-            if (!File.Exists("chats.txt"))
-                File.Create("chats.txt").Close();
             Console.WriteLine("startedLoadData");
-            using (StreamReader sw = new StreamReader("osuusers.txt"))
+            if (!File.Exists("osuusers.txt"))
             {
-                List<osuUser> data = Newtonsoft.Json.JsonConvert.DeserializeObject<osuUser[]>(sw.ReadToEnd()).ToList();
-                Console.WriteLine(data);
-                Variables.osuUsers = data ?? new List<osuUser>();
+                File.Create("osuusers.txt").Close();
             }
-            using (StreamReader sw = new StreamReader("chats.txt"))
+            else
             {
-                List<Types.Chat> data = Newtonsoft.Json.JsonConvert.DeserializeObject<Types.Chat[]>(sw.ReadToEnd()).ToList();
-                Console.WriteLine(data);
-                Variables.chats =  data ?? new List<Chat>();
+                using (StreamReader sw = new StreamReader("osuusers.txt"))
+                {
+                    List<osuUser> data = Newtonsoft.Json.JsonConvert.DeserializeObject<osuUser[]>(sw.ReadToEnd()).ToList();
+                    Console.WriteLine(data);
+                    Variables.osuUsers = data ?? new List<osuUser>();
+                }
+            }
+            if (!File.Exists("chats.txt"))
+            {
+                File.Create("chats.txt").Close();
+            }
+            else
+            {
+                using (StreamReader sw = new StreamReader("chats.txt"))
+                {
+                    List<Types.Chat> data = Newtonsoft.Json.JsonConvert.DeserializeObject<Types.Chat[]>(sw.ReadToEnd()).ToList();
+                    Console.WriteLine(data);
+                    Variables.chats = data ?? new List<Chat>();
+                }
             }
             Console.WriteLine("endedLoadData");
         }
