8ec3f0482184ed4a55b0cb44fa5bab2811b656e3
Shahzod (Linux) <shachzod2004@gmail.com>
2025-07-01 06:30:01 +0200

    Graphics #1

---
+++ SosuBot.Graphics/ProfileCard/OsuProfileCardInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SosuBot.Graphics.ProfileCard
+{
+    public record OsuProfileCardInfo (
+        string Username, 
+        double PP, 
+        double Accuracy,
+        string AvatarUrl);
+}
