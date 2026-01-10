73149311b048e96196bc1b796cde8eaeb6e64a1f
Shahzod <shachzod2004@gmail.com>
2026-01-10 23:22:30 +0100

    Fix render status

--- SosuBot/Services/Handlers/Commands/ReplayRenderCommand.cs
+++ SosuBot/Services/Handlers/Commands/ReplayRenderCommand.cs
@@ -160,7 +160,7 @@ public sealed class ReplayRenderCommand : CommandBase<Message>
 
         var ik = new InlineKeyboardMarkup(new[]
         {
-            InlineKeyboardButton.WithCallbackData("Статус", $"{chatInDatabase!.ChatId} render-status {renderQueueResponse!.JobId}")
+            InlineKeyboardButton.WithCallbackData("Статус", $"render-status {renderQueueResponse!.JobId}")
         });
         await waitMessage.EditAsync(Context.BotClient, $"Текущее количество онлайн рендереров: {onlineRenderersCount}\n\nОчередь: {await _replayRenderService.GetWaitqueueLength(renderQueueResponse!.JobId)}\nИщем свободный рендерер...", replyMarkup: ik);
 
